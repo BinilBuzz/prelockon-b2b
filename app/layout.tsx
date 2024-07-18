52c4629bd8265fdf003bd8845562c092464e40d4
Binil <50130015+BinilBuzz@users.noreply.github.com>
2024-07-18 23:59:13 +0545

    Add files via upload
    changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,7 @@
 import type { Metadata } from "next";
-import Header from "./components/header"
+import Header from "./components/header";
 import Footer from "./components/footer";
 import "./globals.css";
-
-
 
 export const metadata: Metadata = {
   title: "PreLockOn",
